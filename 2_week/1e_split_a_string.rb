46861b5fe3ccaf243afe4e61d8411281dfabe235
Victor Goff <keeperotphones@gmail.com>
2014-09-10 23:19:48 -0400

    per style guides

--- 2_week/1e_split_a_string.rb
+++ 2_week/1e_split_a_string.rb
@@ -1,21 +1,17 @@
-#Core Ruby 55th batch
+# Core Ruby 55th batch
 # Split string Exercise
 # By Chuck Park
-
 
 s = "Welcome to the forum.\nHere you can learn Ruby.\nAlong with other members.\n"
 
 def split_string(in_string)
-
   parsed_string = in_string.split("\n")
-
-  #build line for output
+  # build line for output
   parsed_string.each_with_index do |parsed_line, line_count|
-      yield(parsed_line, line_count)
+    yield(parsed_line, line_count)
   end
-
 end
 
-split_string(s) { |parsed_line, line_count|  puts "Line #{line_count + 1} : #{parsed_line} " }
-
-
+split_string(s) do |parsed_line, line_count|
+  puts "Line #{ line_count + 1} : #{parsed_line} "
+end
