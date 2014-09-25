aeac5b359573a0b974b9b32e53cecac9249ae645
montgco <montgco@gmail.com>
2014-09-25 16:13:30 -0400

    Basics of Dog class
    This area has to be after one blank line, the first line being the
    summary of an idea.  This area being 80 characters wide, and as long as
    ou want it to be.  But describing ideas, not specific code.
    
    The current changes can be described as this (and when your future self
    reads this you will shed a tear of joy for the understanding that you
    have chosen to impart at this time):
    
    Fixes the problems associated with having output in the methods, now
    bark and other methods return a string which can be used as the user
    wants, trying to keep unnecessary opinions of ours out of it.
    
    There is a story used in a HereDoc format that demonstrates what happens
    when puts is used in the method.
    
    Also introduced is the "application block" which does not run when the
    file is loaded as a library (which includes a testing framework) but
    does run when ran directly.

--- 4_week/doggy.rb
+++ 4_week/doggy.rb
@@ -1,56 +1,44 @@
-
-
 class Dog
-
   def initialize(name)
     @name = name
   end
 
-  def bark()
-    puts "#{@name} says Rrrrruff, ruff!"
+  # doctest: I get a name and the dog barks
+  # >> my_dog = Dog.new('Lassie')
+  # >> my_dog.bark
+  # => 'Lassie says Rrrrruff, ruff!'
+  def bark
+    "#{@name} says Rrrrruff, ruff!"
   end
 
-  def eat()
-    puts "Grrrrrrrrrr, stay away from my food!"
+  def eat
+    'Grrrrrrrrrr, stay away from my food!'
   end
 
   def chase_cat
-    puts "Run, run, run! Meeeeeooooooow! Chomp! Gulp! Yum!"
+    'Run, run, run! Meeeeeooooooow! Chomp! Gulp! Yum!'
   end
-
 end
 
-class Doggy < Dog
-@@tricks = Array.new
+# Application guard.  If this file is the file ran directly, run this block of
+# code
+if __FILE__ == $PROGRAM_NAME
+  my_dog = Dog.new('Lassie')
 
-  def teach_trick(type_of_trick)
-    if @@tricks.include?(type_of_trick)
-      puts "#{@name} already knows how to #{type_of_trick}!"
-    else
-       puts "Good job, #{@name} now knows how to #{type_of_trick}!"
-      @@tricks << type_of_trick
-      def Doggy.type_of_trick
-         puts "Look at #{@name} dance!"
-      end
-    end
-  end
+  # The syntax is HereDoc if you want to search it.  It is usable (similar) in
+  # Bash, and other languages besides Ruby
+  story = <<-THE_END
+Once upon a time, I had a dog named %s.  She was a good dog.
 
+I guess I could start with my name.  My name is Timmy.
 
-  def do_trick(type_of_trick)
-    if @@tricks.include?(type_of_trick)
-      puts "Look at #{@name} #{type_of_trick}!"
-    else
-       puts "Sorry, you need to teach #{@name} how to #{type_of_trick} first!"
-    end
+So as I was playing one day, my dog, %s, came to the house and started to bark.
 
-  end
+"%s"
 
+My mom said:  "What?  Timmy fell down a well?"
 
+  THE_END
+
+  puts(story % ['Lassie', 'Lassie', my_dog.bark]).inspect
 end
-
-brooke = Doggy.new "Brooke"
-brooke.bark
-brooke.eat
-brooke.chase_cat
-brooke.teach_trick(:dance)
-brooke.dance
